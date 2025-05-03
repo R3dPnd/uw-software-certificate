17192d1992fbbd9c025c78659f327d901fec6e2f
Preston Harms <prestonharms5@gmail.com>
2025-05-02 23:42:51 -0700

    Refactor benchmark script to simplify runtime capture by removing unnecessary tail command

--- tcss-505-/Assignments/02-Armstrong-Again/benchmark.sh
+++ tcss-505-/Assignments/02-Armstrong-Again/benchmark.sh
@@ -11,7 +11,7 @@ for i in {1..10}
 do
     echo "Running with $i process(es)..."
     # Run the program and capture the runtime from the last line
-    runtime=$(./a1_armstrong.py -n 1000000 -p $i | tail -n 2 | grep "Runtime" | awk '{print $2}')
+    runtime=$(./a1_armstrong.py -n 1000000 -p $i | grep "Runtime" | awk '{print $2}')
     # Append results to CSV
     echo "$i,$runtime" >> benchmark_results.csv
 done
